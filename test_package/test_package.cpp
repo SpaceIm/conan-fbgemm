d9c6745381943607f257dfab794794cc230c6545
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-03-23 18:17:39 +0100

    update test_package

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -8,8 +8,8 @@ int main() {
   std::vector<std::int8_t> bint8{8, 2, 92, -45, 2, -4};
   fbgemm::PackBMatrix<std::int8_t> packedBN(fbgemm::matrix_op_t::NoTranspose, 2,
                                             3, bint8.data(), 3);
-  std::cout << "Offset of the element which was at (2, 3): "
-            << packedBN.addr(2, 3) << std::endl;
+  std::cout << "Offset of the element which was at (1, 2): "
+            << packedBN.addr(1, 2) << std::endl;
 
   return 0;
 }
